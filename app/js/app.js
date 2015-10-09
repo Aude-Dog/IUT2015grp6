fe8ffd9153b76c580a8f126530a503c1dd199e57
AUDE LAMARQUE <lamarque.aude@info-141-00.iutcb.univ-littoral.fr>
2015-10-09 16:32:05 +0200

    premier commit

---
+++ app/js/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/* App Module */
+
+var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatAnimations',
+
+  'phonecatControllers',
+  'phonecatFilters',
+  'phonecatServices'
+]);
+
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+	  when('/phones/modify/:phoneId', {
+		templateUrl: 'partials/phone-modify.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+	  
+	  otherwise({
+        redirectTo: '/phones'
+	  
+      });
+  }]);
